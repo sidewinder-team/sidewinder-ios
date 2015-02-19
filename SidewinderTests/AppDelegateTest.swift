00509e9d721e24416888431dcc307908de7b61e2
robertfmurdock <rmurdock@pillartechnology.com>
2015-02-18 21:18:34 -0500

    Minor tweak to the test; in case the delegate does not exist, the test will now fail.

--- SidewinderTests/AppDelegateTest.swift
+++ SidewinderTests/AppDelegateTest.swift
@@ -3,18 +3,25 @@ import Nimble
 import Sidewinder
 
 class AppDelegateTest: QuickSpec {
+    
     override func spec() {
-        
         describe("When the application registers a device token") {
             it("will create a Job Subscription Service with that token") {
                 let app = UIApplication.sharedApplication()
-                if let delegate = UIApplication.sharedApplication().delegate {
+                expectExists(app.delegate) { delegate in
                     let data = "DeviceToken#1".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
                     delegate.application?(app, didRegisterForRemoteNotificationsWithDeviceToken: data!)
-
                     expect(JobSubscriptionServiceInstance?.DeviceToken).to(equal(data))
                 }
             }
         }
+    }
+}
+
+public func expectExists<T>(potentialValue: T?, then: (value: T) -> Void, file: String = __FILE__, line: UInt = __LINE__) {
+    if let value = potentialValue {
+        then(value: value)
+    } else {
+        XCTFail("Value did not exist.", file: file, line: line)
     }
 }
