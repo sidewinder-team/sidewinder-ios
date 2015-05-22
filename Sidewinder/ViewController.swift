5bda763febfd236c3ad009d928a5821675a238d7
Gregory Haskins <greg@greghaskins.com>
2015-05-21 22:34:08 -0400

    Added a table view. Soon... it shall contain status !

--- Sidewinder/ViewController.swift
+++ Sidewinder/ViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet
+    var tableView: UITableView!
+    
+    let stuff = ["Harry", "Ron", "Hermione"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,23 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stuff.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("RepoStatusCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let value = stuff[indexPath.row]
+        cell.textLabel?.text = value
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
     }
 
 
